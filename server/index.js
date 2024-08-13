1fea84f95c326726b6784239dd310b5f707c9891
vikash <vp3637637@gmail.com>
2024-08-13 17:05:13 +0530

    updated cors

--- server/index.js
+++ server/index.js
@@ -7,15 +7,18 @@ const bodyParser=require('body-parser')
 const cookieParser=require('cookie-parser')
 
 const app=express()
+app.use(cors({
+    origin: "https://charlog.vercel.app", // Allow requests from your frontend domain
+    credentials: true, // Allow cookies to be sent
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allowed methods
+    allowedHeaders: "Content-Type,Authorization", // Allowed headers
+    optionsSuccessStatus: 200 // For legacy browser support
+}));
 app.use(bodyParser.json({ limit: '100mb' }));
 app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 app.use(express.json())
 app.use(cookieParser())
 
-app.use(cors({
-    origin:"https://charlog.vercel.app",
-    credentials:true
-}));
 
 
 mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.DB_PASSWORD}@cluster0.w7gbj.mongodb.net/`).then(()=>{
