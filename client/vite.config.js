df3ae4a12e92364df9fea9687bdffa74ac49c511
vikash <vp3637637@gmail.com>
2024-08-14 18:36:32 +0530

    proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api':'https://charlog-server.vercel.app'
+      '/api': {
+        target: 'https://charlog-server.vercel.app',
+        changeOrigin: true,
+        secure: false,
+      }
     }
   },
   plugins: [react()],
