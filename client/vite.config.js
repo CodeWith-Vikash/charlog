df02675db8eee8c7b04e86a7028341fc4b13c8ee
vikash <vp3637637@gmail.com>
2024-08-14 18:24:00 +0530

    updated code for proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    Proxy:{
-      '/api':'https://charlog-server.vercel.app'
-    }
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://charlog-server.vercel.app',
+        changeOrigin: true,
+        secure: false, // Add this if you are dealing with self-signed certificates
+      },
+    },
   },
   plugins: [react()],
 })
